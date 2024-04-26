3964da61f7a1105fc41930f3bfa03fbcfa2879ee
matmon12 <matveym151@gmail.com>
2024-04-26 23:16:04 +0300

    Revert "first commit"
    This reverts commit a97e0d24e960182058147cae2c20bbdbba71f41f.

--- src/stores/audio.js
+++
@@ -1,9 +0,0 @@
-import { ref } from "vue";
-import { defineStore } from "pinia";
-
-export const useAudioStore = defineStore("audio", () => {
-  const isPlaying = ref(false);
-  const pt = ref(null);
-
-  return { isPlaying, pt };
-});
